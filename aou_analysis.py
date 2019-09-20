5f1595b5ce45c811dfb4de58cb0e6f04439b1494
Danner Claflin <dannerclaflin@gmail.com>
2019-09-20 09:58:58 -0400

    adding the AoU report and module to run it. needs lots of work.

--- aou_analysis.py
+++ aou_analysis.py
@@ -9,3 +9,17 @@ from functools import reduce
 
 import omop_analyze
 import fhir_analyze
+
+def fhir_plot_category_counts(fhir_people):
+    s4s_datatype_totals = {person:{title:len(items) for (title, items) in datatype.items()} for (person, datatype) in fhir_people.items()}
+    s4s_df = pd.DataFrame(s4s_datatype_totals).transpose()
+    return s4s_df.plot(kind='box', figsize=(20,6), logy=True)
+
+def omop_plot_category_counts(omop_people, categories):
+    omop_data_types_per_person = {
+        data_type: [
+            len(person[data_type]) if data_type in person.keys() else 0 for person in omop_people.values()
+        ] for data_type in categories
+    }
+    omop_df_types = pd.DataFrame(omop_data_types_per_person, index=omop_people.values())
+    return omop_df_types.boxplot(figsize=(12,6), showfliers=False)
